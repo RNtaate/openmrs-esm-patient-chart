5456d1cfa083f358b093f4a3fbaf433c4bd8a502
Donald Kibet <chelashawdonald@yahoo.com>
2021-12-21 16:37:18 +0300

    03-983 : Add more columns to care-program table

--- packages/esm-patient-chart-app/src/visit/visit-prompt/end-visit.component.tsx
+++ packages/esm-patient-chart-app/src/visit/visit-prompt/end-visit.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './end-visit.component.scss';
 import { ComposedModal, Button, ModalHeader, ModalBody } from 'carbon-components-react';
 import { useTranslation } from 'react-i18next';
-import { getStartedVisit, showNotification, showToast, updateVisit, useVisit } from '@openmrs/esm-framework';
+import { getStartedVisit, showNotification, showToast, updateVisit, useVisit, parseDate } from '@openmrs/esm-framework';
 import { first } from 'rxjs/operators';
 
 interface EndVisitPromptProps {
@@ -19,7 +19,7 @@ const EndVisitPrompt: React.FC<EndVisitPromptProps> = ({ patientUuid, openModal,
   const endCurrentVisit = () => {
     const endVisitPayload = {
       location: currentVisit.location.uuid,
-      startDatetime: currentVisit.startDatetime,
+      startDatetime: parseDate(currentVisit.startDatetime),
       visitType: currentVisit.visitType.uuid,
       stopDatetime: new Date(),
     };
